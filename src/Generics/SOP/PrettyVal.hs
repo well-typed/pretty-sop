fce49a3ec7d855a270bbb8d6a37bfa74732f0736
Andres Loeh <andres@well-typed.com>
2014-07-08 09:57:19 +0200

    Documentation and Cabal file.

--- src/Generics/SOP/PrettyVal.hs
+++ src/Generics/SOP/PrettyVal.hs
@@ -1,3 +1,8 @@
+-- | Generic pretty-printer.
+--
+-- This module defines a generic function that helps in defining class
+-- instances for the 'PrettyVal' class from the @pretty-show@ package.
+--
 module Generics.SOP.PrettyVal (
     gprettyVal
     -- * Re-exports
@@ -8,6 +13,20 @@ import Text.Show.Pretty
 
 import Generics.SOP
 
+-- | Generic pretty-printer.
+--
+-- This function turns a value into the uniform representation of type
+-- 'Value' that is provided by the @pretty-show@ package. The function
+-- has the suitable type to serve as the default implementation of the
+-- 'prettyVal' function in the 'PrettyVal' class.
+--
+-- If you have a datatype @T@ that is an instance of @generic-sop@'s
+-- 'Generic' and 'HasDatatypeInfo' classes, you can use 'gprettyVal'
+-- as follows:
+--
+-- > instance PrettyVal T where
+-- >   prettyVal = gprettyVal
+--
 gprettyVal :: forall a. (Generic a, HasDatatypeInfo a, All2 PrettyVal (Code a)) => a -> Value
 gprettyVal = gprettyVal' (datatypeInfo (Proxy :: Proxy a)) . from
 
@@ -21,6 +40,11 @@ gprettyVal'' info (SOP sop) =
 
 prettyValFor :: All PrettyVal xs => ConstructorInfo xs -> NP I xs -> K Value xs
 prettyValFor (Constructor n) = K . Con n . hcollapse . hcliftA p (K . prettyVal . unI)
+prettyValFor (Infix n _ _)   = K . aux . hcliftA p (K . prettyVal . unI)
+  where
+    aux :: forall x y. NP (K Value) '[x, y] -> Value
+    aux (K x :* K y :* Nil) = InfixCons x [(n, y)]
+    aux _                   = error "inaccessible"
 prettyValFor (Record n fs)   = K . Rec n . hcollapse . hcliftA2 p aux fs
   where
     aux :: forall a. PrettyVal a => FieldInfo a -> I a -> K (Name, Value) a
