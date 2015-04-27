7656ac84b934061e0230a88ea929782496d3bb5d
Andres Loeh <andres@well-typed.com>
2015-04-27 16:47:42 +0200

    Bring up-to-date with generics-sop-0.2.

--- src/Generics/SOP/PrettyVal.hs
+++ src/Generics/SOP/PrettyVal.hs
@@ -36,7 +36,7 @@ gprettyVal' (Newtype _ _ c)  = gprettyVal'' (c :* Nil)
 
 gprettyVal'' :: (All2 PrettyVal xss, SingI xss) => NP ConstructorInfo xss -> SOP I xss -> Value
 gprettyVal'' info (SOP sop) =
-  unI . hcollapse $ hcliftA2' p prettyValFor info sop
+  hcollapse $ hcliftA2' p prettyValFor info sop
 
 prettyValFor :: All PrettyVal xs => ConstructorInfo xs -> NP I xs -> K Value xs
 prettyValFor (Constructor n) = K . Con n . hcollapse . hcliftA p (K . prettyVal . unI)
